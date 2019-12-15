0b37696184d0735ded682d7765735f6ba2434a26
B. Kelly <bk@ancilla.ca>
2019-12-14 21:12:16 -0500

    Update nix configuration with new build process and sound support

--- etc/doomrl-server.nix
+++ etc/doomrl-server.nix
@@ -8,24 +8,26 @@ in {
     src = self.fetchFromGitHub {
       owner = "toxicfrog";
       repo = "doomrl-server";
-      rev = "368c685f35850e86261c891b2e73a680e8077eb4";
-      sha256 = "1cfp1l8266dbxcvazqhgfxravg0kzdff3c75qjdisakxybjf7jnq";
+      rev = "e81178c05af3f8878592704231eb90a7e55c7fb1";
+      sha256 = "111p1l8266dbxcvazqhgfxravg0kzdff3c75qjdisakxybjf7jnq";
     };
 
-    nativeBuildInputs = with self; [gnumake];
+    nativeBuildInputs = with self; [gnumake git lua5_3];
     buildInputs = with self; [SDL];
-    deps = with self; [python3 telnet less];
+    deps = with self; [python3 telnet less ncurses nano];
 
     phases = [ "unpackPhase" "buildPhase" "installPhase" ];
 
-    buildPhase = ''make -C ttysound'';
+    buildPhase = ''make -C ttysound DRL_SOUND_CONFIG=${self.doomrl}/opt/doomrl/soundhq.lua'';
     installPhase = ''
-      # ls -ltrAh
-      # pwd
-      # echo "out=$out"
-      # stat $out
-      mkdir -p $out/share/
+      mkdir -p "$out/share"
       cp -a . "$out/share/doomrl-server"
+      mkdir -p "$out/share/doomrl-server/www/sfx/"
+      for sfx in ${self.doomrl}/opt/doomrl/wavhq/*.wav; do
+        flac="$(${self.coreutils}/bin/basename "$sfx" | ${self.gnused}/bin/sed -E "s,wav$,flac,")"
+        ${self.ffmpeg}/bin/ffmpeg -hide_banner -loglevel error -i "$sfx" "$out/share/doomrl-server/www/sfx/$flac"
+      done
+      ln -s ${self.doomrl}/opt/doomrl/mp3 "$out/share/doomrl-server/www/music"
     '';
   };
 }
